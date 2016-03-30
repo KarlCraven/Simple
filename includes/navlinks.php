494d72a1ce4ae9b61e1c87f64831a969055f1e15
Karl Craven <kandrewcraven@gmail.com>
2016-03-30 18:32:50 -0400

    Commit before major changes are made.

--- includes/navlinks.php
+++ includes/navlinks.php
@@ -12,14 +12,23 @@
         $thisPage = next($files);
     }
         
-    // set the previous and next link variables using the values of the adjacent indexes
+    // set the previous and next link variables using the values of the adjacent pages
     $prevFile = prev($files);
     $nextFile = next($files);
     $nextFile = next($files);
   
-    // if the current page is the first page, leave the previous variable blank
+    // if the current page is the first page, omit the prev link
+    if ($prevFile[0] != '') {
+        echo('< <a href="' . $prevFile[1] . '">' . $prevFile[0] . '</a>');
+    }
     
-    // if the current page is the last page, leave the next variable blank
+    // if this page is not the first or last
+    if ($prevFile[0] != '' && $nextFile[0] != '') {
+        echo(' | ');
+    }
     
-    echo('< <a href="' . $prevFile[1] . '">' . $prevFile[0] . '</a>  |  <a href="' . $nextFile[1] . '">' . $nextFile[0] . '</a> >');
+    // if the current page is the last page, omit the next link
+    if ($nextFile[0] != '') {
+        echo('<a href="' . $nextFile[1] . '">' . $nextFile[0] . '</a> >');
+    }
 ?>
